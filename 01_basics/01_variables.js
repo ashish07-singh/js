7e38244caeccd25ee53df52fee7a19c95667cb11
ashish07-singh <ashishsinghrawat649@gmail.com>
2024-08-03 20:47:43 +0000

    variable changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,27 @@
+const account_Id = 12345; //const canot be changed
+
+    //declaring VARIABLE
+let account_Email = "ashishsinghrawat649@gmail.com"
+var accountPassword = "786033"
+accountCity = "jaipur"
+
+   
+    //changing them
+
+account_Email= "askddgfk@gmail.com"
+accountPassword="46464646"
+
+//variable can also be used without defining its datatype 
+accountCity="delhi"  
+let account_State;  // if we print it the output would be undefined  *****
+
+console.log(account_Id);
+
+
+console.table([account_Id,account_Email,accountPassword,accountCity,account_State])  //used to print in table
+
+/*    NOTES    
+
+  -> prefer not to use ( VAR ) bcuz of issue in block scope and functional scope
+
+*/
