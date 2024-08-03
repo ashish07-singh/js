619be9cbe9f0ee40a99a599fb1faacf75b5bf63a
ashish07-singh <ashishsinghrawat649@gmail.com>
2024-08-03 22:40:32 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+/*
+let score = 33
+
+console.log(typeof score)  //output - number
+console.log(typeof(score))  //output - number
+
+*/
+
+
+
+/*typecast or coversion */
+
+let score = "33asd"
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); //op - number
+console.log(valueInNumber);  //op- NaN     ***** it is a special type
+
+
+/**
+ * convertion
+ * "33" -> 33
+ * "33asd" -> NaN
+ * true - 1/ false- 0
+ * null - 0
+ * undefined - nan
+ */
