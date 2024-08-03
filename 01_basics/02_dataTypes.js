a0976dd55d180d6b759abc466329e10062201cb3
ashish07-singh <ashishsinghrawat649@gmail.com>
2024-08-03 21:17:37 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3 + 3)   // we are using node js, not browser
+
+console.log(3+4); console.log("ashish");  // but it has a code readibility problem
+
+// DataTypes
+
+let name = "ashish singh rawat" // string
+let age = 24   //number
+let isLoggedIn = false   //boolean
+
+
+/* primitive datatypes 
+
+number -> 2 to power 53
+bigint 
+string = "" 
+boolean -> true/false
+//null = standalone value 
+// undefined ==> abhi value assign nhi hui h
+symbols ==> unique
+
+
+*/
+
+    //object
+
+    console.log(typeof "ashish") //output -> string
+
+    console.log(typeof null) //output -> object         *****  bcoz null is a object
+    console.log(typeof undefined) //output -> undefined  ***** bcoz undefined apne aap main ek type hai
