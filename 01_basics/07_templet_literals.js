7522913056fc376e0d373bd7b57014d9d4b9818e
ashish07-singh <ashishsinghrawat649@gmail.com>
2024-08-10 15:33:26 +0000

    printing string and value together

---
+++ 01_basics/07_templet_literals.js
@@ -0,0 +1,10 @@
+//     use of BACKTICK (``) - it is present at just below thw ESC button
+
+let age = 24; 
+let exp = 4;
+
+console.log(`My age is ${age} and exp is ${exp}`);            //      ***** 
+
+// -> also used in printing (")
+console.log(`ashish's son`);    // with bacttick ``
+console.log("ashish\'s son");  // with escape sequence  \n , \" , \'
