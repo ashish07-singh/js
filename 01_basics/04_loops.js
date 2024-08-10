02f6bf9f02ea293e64b993a45beefadeccab419b
ashish07-singh <ashishsinghrawat649@gmail.com>
2024-08-10 08:11:50 +0000

    loops

---
+++ 01_basics/04_loops.js
@@ -0,0 +1,28 @@
+for(let i=1; i<=10; i++){
+    process.stdout.write(i + " ");
+    
+}
+console.log();
+
+
+//...while loop
+
+let a = 1;
+while(a<=10){
+    console.log(a);
+    a++;
+}
+
+//do while loop
+
+let b= 1;
+do{
+    console.log(b);
+    b++;
+
+}while(b<=6)
+
+
+// to print array
+let arr = [1, 2, 3, 4, 5];
+console.log(arr.join(" "));
