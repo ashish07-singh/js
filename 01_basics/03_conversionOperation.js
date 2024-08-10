dd806f541322330244feb7f9e73e0dac1b079970
ashish07-singh <ashishsinghrawat649@gmail.com>
2024-08-10 08:11:36 +0000

    triple eqalto

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,3 +24,18 @@ console.log(valueInNumber);  //op- NaN     ***** it is a special type
  * null - 0
  * undefined - nan
  */
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+console.log(2+2)
+console.log(2**3)  //power operator
+console.log(2%3)
+
+
+
+
+// (===) - it campare value and its type also
+// (==) - it only compare which is visible i.e.. 5 == '5' -true
