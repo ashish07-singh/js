06df0d366042ce75261cb2fa3a323db758c27606
ashish07-singh <ashishsinghrawat649@gmail.com>
2024-08-16 17:11:09 +0000

    function

--- 01_basics/09_function.js
+++ 01_basics/09_function.js
@@ -16,3 +16,21 @@ function eqn(a,b){
 let a = -3;
 let b = 4;
 console.log(eqn(a,b));    // eqn () ek valuse return karke degi jo  ki print karani hogi console.log se 
+
+
+// other function
+console.log(Math.abs(-7))
+console.log(Math.abs(-7.5))
+console.log(Math.max(5,1))
+console.log(Math.max(7,1,8,2,4,6,3))   //only in javascript to get maximum out of multiple number
+
+console.log(Math.pow(2,3))
+console.log(Math.pow(2.3,2))   //only in js
+console.log(Math.log(2.81))
+
+console.log(Math.sqrt(64))   
+console.log(Math.cbrt(529))
+
+//floor , ceil
+
+console.log(Math.random())  // ge
